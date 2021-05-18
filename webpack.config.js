20ffef5b0e61e635f2035b786cf8a05df6945a92
mono-tmt <tomita@monosus.co.jp>
2021-05-18 19:06:06 +0900

    :+1: Add server config for webpack
    t

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,34 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+const serverConfig = {
+	mode: process.env.NODE_ENV || 'development',
+	entry: './src/server/server.ts',
+	module: {
+		rules: [
+			{
+				test: /\.ts?$/,
+				loader: 'ts-loader',
+				exclude: /node_modules/,
+				options: {
+					configFile: 'tsconfig.server.json'
+				}
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.ts', '.js']
+	},
+	output: {
+		filename: 'server.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	target: 'node',
+	node: {
+		__dirname: false
+	},
+	externals: [nodeExternals()]
+};
 
 const clientConfig = {
 	mode: process.env.NODE_ENV || 'development',
@@ -24,4 +54,4 @@ const clientConfig = {
 		path: path.resolve(__dirname, 'public/js')
 	}
 };
-module.exports = [clientConfig];
+module.exports = [clientConfig, serverConfig];
